09f04d297982a96148315cd9850f50788ee17f8c
Vicky AS <vickyhuang808@gmail.com>
2024-05-24 17:59:40 +0700

    edit file

--- repository/person.go
+++ repository/person.go
@@ -6,7 +6,7 @@ import (
 )
 
 func GetAllPerson(db *sql.DB) (results []structs.Person, err error) {
-	sql := "SELECT * FROM person"
+	sql := "SELECT * FROM person;"
 	rows, err := db.Query(sql)
 
 	if err != nil {
@@ -28,17 +28,17 @@ func GetAllPerson(db *sql.DB) (results []structs.Person, err error) {
 }
 
 func InsertPerson(db *sql.DB, person structs.Person) (err error) {
-	sql := "INSERT INTO person(id,first_name,last_name) VALUES ($1,$2,$3)"
+	sql := "INSERT INTO person(id,first_name,last_name) VALUES ($1,$2,$3);"
 	errs := db.QueryRow(sql, person.ID, person.FirstName, person.LastName)
 	return errs.Err()
 }
 func UpdatePerson(db *sql.DB, person structs.Person) (err error) {
-	sql := "UPDATE person SET first_name=$1,last_name=$2 WHERE id=$3"
+	sql := "UPDATE person SET first_name=$1,last_name=$2 WHERE id=$3;"
 	errs := db.QueryRow(sql, person.FirstName, person.LastName, person.ID)
 	return errs.Err()
 }
 func DeletePerson(db *sql.DB, person structs.Person) (err error) {
-	sql := "DELETE FROM person WHERE id=$1"
+	sql := "DELETE FROM person WHERE id=$1;"
 	errs := db.QueryRow(sql, person.ID)
 	return errs.Err()
 }
