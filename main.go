fc4baa5f0339c813de28b2a6685df4579d53ac8d
Vicky AS <vickyhuang808@gmail.com>
2024-05-24 16:32:32 +0700

    membuat crud dengan gin framework

--- main.go
+++ main.go
@@ -3,18 +3,22 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"os"
+	"practice/controllers"
 	"practice/database"
 
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
-const (
-	host     = "localhost"
-	port     = 5432
-	user     = "postgres"
-	password = "password"
-	dbname   = "practice"
-)
+// const (
+// 	host     = "localhost"
+// 	port     = 5432
+// 	user     = "postgres"
+// 	password = "password"
+// 	dbname   = "practice"
+// )
 
 var (
 	DB  *sql.DB
@@ -23,8 +27,16 @@ var (
 
 func main() {
 
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
-		host, port, user, password, dbname)
+	err = godotenv.Load("config/.env")
+
+	if err != nil {
+		fmt.Println("failed load file environment")
+	} else {
+		fmt.Println("success read file environment")
+	}
+
+	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"))
 
 	DB, err = sql.Open("postgres", psqlInfo)
 
@@ -39,5 +51,13 @@ func main() {
 	database.DbMigrate(DB)
 
 	defer DB.Close()
+
+	router := gin.Default()
+	router.GET("/persons", controllers.GetAllPerson)
+	router.POST("/persons", controllers.InsertPerson)
+	router.PUT("/persons/:id", controllers.UpdatePerson)
+	router.DELETE("/persons/:id", controllers.DeletePerson)
+
+	router.Run("localhost:8080")
 
 }
